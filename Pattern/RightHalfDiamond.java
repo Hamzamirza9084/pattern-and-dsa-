c04fa6b8ea8dfb7b5ed608dabef63a8b01d34b0e
Hamzamirza9084 <hamzamirza9084@gmail.com>
2026-05-07 12:58:49 +0530

    RightHalfDiamond

---
+++ Pattern/RightHalfDiamond.java
@@ -0,0 +1,27 @@
+public class RightHalfDiamond
+{
+    public static void main(String S[])
+    {
+        int n=4,i,j,k;
+
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+           
+            System.out.println();
+        }
+       
+        for(i=n-1;i>=1;i--)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+           
+            System.out.println();
+        }
+    }
+}

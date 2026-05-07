01c122b7a59769ca771cae86c118c0be0a0075fa
Hamzamirza9084 <hamzamirza9084@gmail.com>
2026-05-07 11:59:46 +0530

    Pyramid2StarRev

---
+++ Pattern/Pyramid2StarRev.java
@@ -0,0 +1,20 @@
+public class Pyramid2StarRev
+{
+    public static void main(String S[])
+    {
+        int n=5,i,j,k;
+
+        for(i=n;i>=1;i--)
+        {
+            for(j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=1;k<=i;k++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
